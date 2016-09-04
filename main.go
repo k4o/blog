6f25d9bf5940907362f16295f2c344aaadf76935
konono <kono@kononet.net>
2016-09-04 14:22:12 +0900

    add middleware

--- main.go
+++ main.go
@@ -1,16 +1,23 @@
 package main
- 
+
 import (
-    "net/http"
- 
-    "github.com/labstack/echo"
-    "github.com/labstack/echo/engine/standard"
+	"net/http"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/engine/standard"
+	"github.com/labstack/echo/middleware"
 )
- 
+
 func main() {
-    e := echo.New()
-    e.GET("/", func(c echo.Context) error {
-        return c.String(http.StatusOK, "Hello, World!")
-    })
-    e.Run(standard.New(":1323"))
+	// Echoインスタンス作成
+	e := echo.New()
+
+	// ミドルウェアの宣言
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Run(standard.New(":1323"))
 }
